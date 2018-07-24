082e039396a7fcc20bf8336b9c4fa2d9a8407d4e
ievamisk <ieva.misk@gmail.com>
2018-07-24 20:54:41 +0300

    Initial setup

---
+++ trip-planner-fe/src/components/hello.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+        <Text>Hello world!</Text>
+    );
+  }
+}
+
+export { HelloWorldApp }
